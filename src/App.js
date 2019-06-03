fb70a4a9cc18aa8e29e1dc1123b16e41e037a577
Mike <michael.barbone@gmail.com>
2019-06-02 23:22:58 -0400

    Change file names and add basic placeholder footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
-import Header from "./components/Header";
+import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Card from "./components/Card";
+import Footer from "./components/Footer";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Navbar />
       <Hero />
+      <Card />
+      <Footer />
     </div>
   );
 }
