0a9c23132d211e6ab7a98df716ad59422fbc3b06
Mike <michael.barbone@gmail.com>
2019-06-03 15:37:25 -0400

    Create footer, add github logo with link to github and css on hover effect

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,7 +4,19 @@ import "./style.css";
 export default function Footer() {
   return (
     <div>
-      <div className="footer bottom" />
+      <div className="footer">
+        <div className="footer text-center">
+          <a
+            id="footer-text"
+            href="https://github.com/MBarbone"
+            target="_blank"
+            rel="noopener noreferrer"
+            alt="Github Link"
+          >
+            <i class="fa fa-github fa-3x" aria-hidden="true" />
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
