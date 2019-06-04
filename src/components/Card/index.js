a3e7f9559a88960c350dc00cd4687879bff0c784
Mike <michael.barbone@gmail.com>
2019-06-03 22:27:58 -0400

    Create array of objects with images and id's and renamed all image files to correspong with picture

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,22 +1,101 @@
 import React from "react";
 import "./style.css";
 
-export default function Card() {
+export default function Card(props) {
   return (
     <div>
       <div className="container">
-        <div className="card" />
-        <div className="card" />
-        <div className="card" />
-        <div className="card" />
-        <div className="card" />
-        <div className="card" />
-        <div className="card" />
-        <div className="card" />
-        <div className="card" />
-        <div className="card" />
-        <div className="card" />
-        <div className="card" />
+        <div className="card">
+          <img
+            src={require("../../img/540254_orig.jpg")}
+            className="img-fluid"
+            alt={props.imageUrl}
+          />
+        </div>
+        <div className="card">
+          <img
+            src={require("../../img/540254_orig.jpg")}
+            className="img-fluid"
+            alt={props.imageUrl}
+          />
+        </div>
+        <div className="card">
+          <img
+            src={require("../../img/540254_orig.jpg")}
+            className="img-fluid"
+            alt={props.imageUrl}
+          />
+        </div>
+        <div className="card">
+          <img
+            src={require("../../img/540254_orig.jpg")}
+            className="img-fluid"
+            alt={props.imageUrl}
+          />
+        </div>
+        <div className="card">
+          <img
+            src={require("../../img/540254_orig.jpg")}
+            className="img-fluid"
+            alt={props.imageUrl}
+          />
+        </div>
+        <div className="card">
+          <img
+            src={require("../../img/540254_orig.jpg")}
+            className="img-fluid"
+            alt={props.imageUrl}
+          />
+        </div>
+        <div className="card">
+          <img
+            src={require("../../img/540254_orig.jpg")}
+            className="img-fluid"
+            alt={props.imageUrl}
+          />
+        </div>
+        <div className="card">
+          <img
+            src={require("../../img/540254_orig.jpg")}
+            className="img-fluid"
+            alt={props.imageUrl}
+          />
+        </div>
+        <div className="card">
+          <img
+            src={require("../../img/540254_orig.jpg")}
+            className="img-fluid"
+            alt={props.imageUrl}
+          />
+        </div>
+        <div className="card">
+          <img
+            src={require("../../img/540254_orig.jpg")}
+            className="img-fluid"
+            alt={props.imageUrl}
+          />
+        </div>
+        <div className="card">
+          <img
+            src={require("../../img/540254_orig.jpg")}
+            className="img-fluid"
+            alt={props.imageUrl}
+          />
+        </div>
+        <div className="card">
+          <img
+            src={require("../../img/540254_orig.jpg")}
+            className="img-fluid"
+            alt={props.imageUrl}
+          />
+        </div>
+        <div className="card">
+          <img
+            src={require("../../img/540254_orig.jpg")}
+            className="img-fluid"
+            alt={props.imageUrl}
+          />
+        </div>
       </div>
     </div>
   );
